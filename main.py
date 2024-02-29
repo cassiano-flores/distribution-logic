233f741c962de5dd87fc4333d845c90b0b95aadc
cassiano-flores <cassiano.flores@hotmail.com>
2024-02-29 18:55:43 -0300

    english version

--- main.py
+++ main.py
@@ -1,59 +1,60 @@
 import requests
+import math
 
-# Definição dos Ativos
-ativos = {
-    'BBAS3':  {'quantidade': 5,          'porcentagem': (0.4 * 0.28 * (1 / 3)),  'tipo': 'BR', 'preco': 0},
-    'BBDC3':  {'quantidade': 35,         'porcentagem': (0.4 * 0.28 * (1 / 3)),  'tipo': 'BR', 'preco': 0},
-    'SANB3':  {'quantidade': 49,         'porcentagem': (0.4 * 0.28 * (1 / 3)),  'tipo': 'BR', 'preco': 0},
-    'AESB3':  {'quantidade': 20,         'porcentagem': (0.4 * 0.45 * (1 / 5)),  'tipo': 'BR', 'preco': 0},
-    'AURE3':  {'quantidade': 53,         'porcentagem': (0.4 * 0.45 * (1 / 5)),  'tipo': 'BR', 'preco': 0},
-    'CMIG4':  {'quantidade': 46,         'porcentagem': (0.4 * 0.45 * (1 / 5)),  'tipo': 'BR', 'preco': 0},
-    'TAEE3':  {'quantidade': 68,         'porcentagem': (0.4 * 0.45 * (1 / 5)),  'tipo': 'BR', 'preco': 0},
-    'TRPL4':  {'quantidade': 27,         'porcentagem': (0.4 * 0.45 * (1 / 5)),  'tipo': 'BR', 'preco': 0},
-    'BBSE3':  {'quantidade': 10,         'porcentagem': (0.4 * 0.18 * (1 / 2)),  'tipo': 'BR', 'preco': 0},
-    'CXSE3':  {'quantidade': 20,         'porcentagem': (0.4 * 0.18 * (1 / 2)),  'tipo': 'BR', 'preco': 0},
-    'SAPR3':  {'quantidade': 41,         'porcentagem': (0.4 * 0.09 * (1)),      'tipo': 'BR', 'preco': 0},
-    'KNRI11': {'quantidade': 4,          'porcentagem': (0.25 * 0.5 * (1 / 2)),  'tipo': 'BR', 'preco': 0},
-    'MXRF11': {'quantidade': 70,         'porcentagem': (0.25 * 0.5 * (1 / 2)),  'tipo': 'BR', 'preco': 0},
-    'HGLG11': {'quantidade': 1,          'porcentagem': (0.25 * 0.18 * (1 / 2)), 'tipo': 'BR', 'preco': 0},
-    'VILG11': {'quantidade': 6,          'porcentagem': (0.25 * 0.18 * (1 / 2)), 'tipo': 'BR', 'preco': 0},
-    'BRCR11': {'quantidade': 10,         'porcentagem': (0.25 * 0.18 * (1 / 2)), 'tipo': 'BR', 'preco': 0},
-    'HGRE11': {'quantidade': 5,          'porcentagem': (0.25 * 0.18 * (1 / 2)), 'tipo': 'BR', 'preco': 0},
-    'MALL11': {'quantidade': 1,          'porcentagem': (0.25 * 0.14 * (1 / 2)), 'tipo': 'BR', 'preco': 0},
-    'XPML11': {'quantidade': 2,          'porcentagem': (0.25 * 0.14 * (1 / 2)), 'tipo': 'BR', 'preco': 0},
-    'VOO':    {'quantidade': 0,          'porcentagem': (0.2 * 0.5),             'tipo': 'US', 'preco': 0},
-    'BIL':    {'quantidade': 0,          'porcentagem': (0.2 * 0.30),            'tipo': 'US', 'preco': 0},
-    'VNQ':    {'quantidade': 0,          'porcentagem': (0.2 * 0.20),            'tipo': 'US', 'preco': 0},
-    'BTC':    {'quantidade': 0.00318941, 'porcentagem': (0.05 * 0.8),            'tipo': 'CR', 'preco': 0},
-    'ETH':    {'quantidade': 0.0205375,  'porcentagem': (0.05 * 0.2),            'tipo': 'CR', 'preco': 0},
-    'RE':     {'quantidade': 1000,       'porcentagem': (0.1),                   'tipo': 'BR', 'preco': 1},
+# My assets
+assets = {
+    'BBAS3':  {'quantity': 5,          'targetPercent': (0.4 * 0.28 * (1 / 3)),  'type': 'BR', 'price': 0},
+    'BBDC3':  {'quantity': 35,         'targetPercent': (0.4 * 0.28 * (1 / 3)),  'type': 'BR', 'price': 0},
+    'SANB3':  {'quantity': 49,         'targetPercent': (0.4 * 0.28 * (1 / 3)),  'type': 'BR', 'price': 0},
+    'AESB3':  {'quantity': 20,         'targetPercent': (0.4 * 0.45 * (1 / 5)),  'type': 'BR', 'price': 0},
+    'AURE3':  {'quantity': 53,         'targetPercent': (0.4 * 0.45 * (1 / 5)),  'type': 'BR', 'price': 0},
+    'CMIG4':  {'quantity': 46,         'targetPercent': (0.4 * 0.45 * (1 / 5)),  'type': 'BR', 'price': 0},
+    'TAEE3':  {'quantity': 68,         'targetPercent': (0.4 * 0.45 * (1 / 5)),  'type': 'BR', 'price': 0},
+    'TRPL4':  {'quantity': 27,         'targetPercent': (0.4 * 0.45 * (1 / 5)),  'type': 'BR', 'price': 0},
+    'BBSE3':  {'quantity': 10,         'targetPercent': (0.4 * 0.18 * (1 / 2)),  'type': 'BR', 'price': 0},
+    'CXSE3':  {'quantity': 20,         'targetPercent': (0.4 * 0.18 * (1 / 2)),  'type': 'BR', 'price': 0},
+    'SAPR3':  {'quantity': 41,         'targetPercent': (0.4 * 0.09 * (1)),      'type': 'BR', 'price': 0},
+    'KNRI11': {'quantity': 4,          'targetPercent': (0.25 * 0.5 * (1 / 2)),  'type': 'BR', 'price': 0},
+    'MXRF11': {'quantity': 70,         'targetPercent': (0.25 * 0.5 * (1 / 2)),  'type': 'BR', 'price': 0},
+    'HGLG11': {'quantity': 1,          'targetPercent': (0.25 * 0.18 * (1 / 2)), 'type': 'BR', 'price': 0},
+    'VILG11': {'quantity': 6,          'targetPercent': (0.25 * 0.18 * (1 / 2)), 'type': 'BR', 'price': 0},
+    'BRCR11': {'quantity': 10,         'targetPercent': (0.25 * 0.18 * (1 / 2)), 'type': 'BR', 'price': 0},
+    'HGRE11': {'quantity': 5,          'targetPercent': (0.25 * 0.18 * (1 / 2)), 'type': 'BR', 'price': 0},
+    'MALL11': {'quantity': 1,          'targetPercent': (0.25 * 0.14 * (1 / 2)), 'type': 'BR', 'price': 0},
+    'XPML11': {'quantity': 2,          'targetPercent': (0.25 * 0.14 * (1 / 2)), 'type': 'BR', 'price': 0},
+    'VOO':    {'quantity': 0,          'targetPercent': (0.2 * 0.5),             'type': 'US', 'price': 0},
+    'BIL':    {'quantity': 0,          'targetPercent': (0.2 * 0.30),            'type': 'US', 'price': 0},
+    'VNQ':    {'quantity': 0,          'targetPercent': (0.2 * 0.20),            'type': 'US', 'price': 0},
+    'BTC':    {'quantity': 0.00318941, 'targetPercent': (0.05 * 0.8),            'type': 'CR', 'price': 0},
+    'ETH':    {'quantity': 0.0205375,  'targetPercent': (0.05 * 0.2),            'type': 'CR', 'price': 0},
+    'RE':     {'quantity': 1000,       'targetPercent': (0.1),                   'type': 'BR', 'price': 1},
 }
 
 
-# Função para escrever o relatório de aportes
-def print_report(aportes):
-    with open("report.txt", "w") as arquivo:
-        for ativo, propriedades in aportes.items():
-            nome_ativo = ativo.ljust(10)
-            quantidade = str(propriedades['quantidade']).ljust(10)
-            valor = str(propriedades['valor']).ljust(10)
-            linha = f"Ativo: {nome_ativo} Quantidade: {quantidade} Valor: {valor}\n"
-            arquivo.write(linha)
+# Write the contribution report
+def print_report(contribution):
+    with open("report.txt", "w") as file:
+        for asset, properties in contribution.items():
+            asset_name = asset.ljust(10)
+            quantity = "{:.3e}".format(round((properties['quantity']), 3)).ljust(10)
+            value = "{:.3f}".format(properties['value']).ljust(10)
+            line = f"Asset: {asset_name} Quantity: {quantity} Value: {value}\n"
+            file.write(line)
 
 
-# Função para escrever o portfólio
-def print_portfolio(ativos):
-    with open("portfolio.txt", "w") as arquivo:
-        for ativo, propriedades in ativos.items():
-            nome_ativo = ativo.ljust(10)
-            quantidade = str(propriedades['quantidade']).ljust(10)
-            preco = "{:.3f}".format(propriedades['preco']).ljust(10)
-            valor = "{:.3f}".format(propriedades['quantidade'] * propriedades['preco']).ljust(10)
-            linha = f"Ativo: {nome_ativo} Quantidade: {quantidade} Preco: {preco} Valor: {valor}\n"
-            arquivo.write(linha)
+# Write the portfolio report
+def print_portfolio(assets):
+    with open("portfolio.txt", "w") as file:
+        for asset, properties in assets.items():
+            asset_name = asset.ljust(10)
+            quantity = "{:.3e}".format(round((properties['quantity']), 3)).ljust(10)
+            price = "{:.3f}".format(properties['price']).ljust(10)
+            value = "{:.3f}".format(properties['quantity'] * properties['price']).ljust(10)
+            line = f"Asset: {asset_name} Quantity: {quantity} Price: {price} Value: {value}\n"
+            file.write(line)
 
 
-# Função para obter o valor atual do dólar em reais
+# Get the current dollar value in reais
 def get_usd_to_brl_price():
     url = "https://economia.awesomeapi.com.br/last/USD-BRL"
     response = requests.get(url)
@@ -61,105 +62,103 @@ def get_usd_to_brl_price():
     return float(data["USDBRL"]["bid"])
 
 
-# Função para obter os preços atuais dos ativos brasileiros e atualizar no dicionário 'ativos'
-def update_brazilian_assets_prices():
+# Get current prices of BR assets
+def update_br_assets_prices():
     url = "https://brapi.dev/api/quote/list?sortBy=name&sortOrder=asc&token=2RpfSrYsBy4i23T6TLdZa2"
     response = requests.get(url)
     data = response.json()
 
     for stock in data['stocks']:
-        if stock['stock'] in ativos and ativos[stock['stock']]['tipo'] == 'BR':
-            ativos[stock['stock']]['preco'] = stock['close']
+        if stock['stock'] in assets and assets[stock['stock']]['type'] == 'BR':
+            assets[stock['stock']]['price'] = stock['close']
 
 
-# Função para obter o preço atual de um ativo estrangeiro e atualizar no dicionário 'ativos'
-def update_foreign_assets_prices(symbol):
-    api_key = 'SUA_CHAVE_DE_API'
+# Get the current price of a US asset
+def update_us_assets_prices(symbol):
+    api_key = 'YOUR_API_KEY'
     url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={api_key}'
     response = requests.get(url)
     data = response.json()
     if 'Global Quote' in data:
         price = float(data['Global Quote']['05. price'])
-        ativos[symbol]['preco'] = (price * get_usd_to_brl_price())
+        assets[symbol]['price'] = (price * get_usd_to_brl_price())
 
 
-# Função para obter o preço atual de uma crypto e atualizar no dicionário 'ativos'
-def update_cryptos_prices(symbol):
+# Get the current price of a crypto
+def update_crypto_prices(symbol):
     url = f'https://economia.awesomeapi.com.br/last/{symbol}-BRL'
     response = requests.get(url)
     data = response.json()
-    ativos[symbol]['preco'] = float(data[f'{symbol}BRL']['bid'])
+    assets[symbol]['price'] = float(data[f'{symbol}BRL']['bid'])
 
 
-# Função para calcular e realizar aportes conforme as porcentagens desejadas
-def realizar_aportes(aporte_total):
-    aportes = {}
+# Calculate and make contributions according to the desired percentages
+def distribute_contributions(total_contribution):
+    new_assets = {}
 
-    while aporte_total > 0:
-        # Calcula o valor total da carteira
-        valor_total_carteira = sum(propriedades['quantidade'] * propriedades['preco'] for propriedades in ativos.values())
+    while total_contribution > 0:
+        # Calculates the total value of the portfolio
+        total_value_portfolio = sum(properties['quantity'] * properties['price'] for properties in assets.values())
 
-        # Calcula as porcentagens atuais de cada ativo
-        porcentagens_atuais = {ativo: (propriedades['quantidade'] * propriedades['preco'] / valor_total_carteira) * 100
-                               for ativo, propriedades in ativos.items()}
+        # Calculates the current percentages of each asset
+        current_percentages = {asset: (properties['quantity'] * properties['price'] / total_value_portfolio) * 100
+                               for asset, properties in assets.items()}
 
-        # Encontra o ativo com a maior diferença percentual
-        ativo_maior_diferenca = max(ativos.items(), key=lambda x: x[1]['porcentagem'] * 100 - porcentagens_atuais[x[0]])
+        # Finds the asset with the highest percentage difference
+        asset_highest_difference = max(assets.items(), key=lambda x: x[1]['targetPercent'] * 100 - current_percentages[x[0]])
 
-        # Verifica o tique mínimo
-        tique_minimo = 1 if ativo_maior_diferenca[1]['tipo'] == 'BR' else 0.01 if ativo_maior_diferenca[1]['tipo'] == 'US' else 0.0001
+        # Get the minimum tick
+        minimum_tick = 1 if asset_highest_difference[1]['type'] == 'BR' else 0.01 if asset_highest_difference[1]['type'] == 'US' else 0.0001
 
-        # Calcula o valor financeiro a ser aportado
-        valor_aportar = ativo_maior_diferenca[1]['preco'] * tique_minimo
+        # Calculates the financial amount to be contributed
+        amount_to_contribute = asset_highest_difference[1]['price'] * minimum_tick
 
-        # Verifica se o valor financeiro a ser aportado é maior que o aporte total
-        if valor_aportar > aporte_total:
+        # Checks if the financial amount to be contributed is greater than the total contribution
+        if amount_to_contribute > total_contribution:
             break
 
-        # Atualiza o aporte total
-        aporte_total -= valor_aportar
+        # Update the total contribution
+        total_contribution -= amount_to_contribute
 
-        # Adiciona o ativo nos aportes
-        if ativo_maior_diferenca[0] not in aportes:
-            aportes[ativo_maior_diferenca[0]] = {'quantidade': tique_minimo, 'valor': valor_aportar}
+        # Add the new asset
+        if asset_highest_difference[0] not in new_assets:
+            new_assets[asset_highest_difference[0]] = {'quantity': minimum_tick, 'value': amount_to_contribute}
         else:
-            aportes[ativo_maior_diferenca[0]]['quantidade'] += tique_minimo
-            aportes[ativo_maior_diferenca[0]]['valor'] += valor_aportar
+            new_assets[asset_highest_difference[0]]['quantity'] += minimum_tick
+            new_assets[asset_highest_difference[0]]['value'] += amount_to_contribute
 
-        # Atualiza a quantidade em "ativos"
-        ativos[ativo_maior_diferenca[0]]['quantidade'] += tique_minimo
+        # Update assets quantity
+        assets[asset_highest_difference[0]]['quantity'] += minimum_tick
 
-    # Já escreve no txt os aportes a realizar
-    print_report(aportes)
+    print_report(new_assets)
 
 
-# Função principal
+# Main function
 def main():
-    # Solicitar o valor do aporte ao usuário
-    aporte_total = float(input("Digite o valor do aporte: "))
-    print("Calculando...")
+    # Request the amount of the contribution from the user
+    total_contribution = float(input("Enter the contribution amount: "))
+    print("Calculating...")
 
-    # Atualizar preços BR
-    update_brazilian_assets_prices()
+    # Update BR prices
+    update_br_assets_prices()
 
-    # Atualizar preços US
-    for symbol, propriedades in ativos.items():
-        if propriedades['tipo'] == 'US':
-            update_foreign_assets_prices(symbol)
+    # Update US prices
+    for symbol, properties in assets.items():
+        if properties['type'] == 'US':
+            update_us_assets_prices(symbol)
 
-    # Atualizar preços Crypto
-    for symbol, propriedades in ativos.items():
-        if propriedades['tipo'] == 'CR':
-            update_cryptos_prices(symbol)
+    # Update crypto prices
+    for symbol, properties in assets.items():
+        if properties['type'] == 'CR':
+            update_crypto_prices(symbol)
 
-    # Realizar os aportes
-    realizar_aportes(aporte_total)
+    # Calculate the contributions
+    distribute_contributions(total_contribution)
 
-    print_portfolio(ativos)
+    print_portfolio(assets)
 
-    print("Finalizado")
+    print("Finished")
 
 
-# Executar a função principal
 if __name__ == "__main__":
     main()
